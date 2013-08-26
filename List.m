90f6ab2948cf6dfdfbe3ab994501c60e15225ee5
Julius F. Heins <jfheins@gmail.com>
2013-08-26 15:00:56 +0200

    Angefangen mit der Liste

---
+++ List.m
@@ -0,0 +1,18 @@
+classdef List
+    %LIST Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties(SetAccess = protected)
+        Capacity;
+        Count
+    end
+    
+    properties(SetAccess = protected)
+        Capacity;
+    end
+    
+    methods
+    end
+    
+end
+
