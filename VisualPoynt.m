85edefb5e42391d8d3ed6d2d15771e5bcd0bcfed
MWH <mw.h@outlook.com>
2013-08-27 14:30:24 +0200

    Wellenfronten in VisualPoynt.m eingezeichnet

--- VisualPoynt.m
+++ VisualPoynt.m
@@ -1,19 +1,42 @@
 run('init.m')
 clc
+diskreteQuiver = 40;
+anzWellen = 50;
+diskreteWelle = 1000;
+
 wz = @(z) param.w0 * sqrt(1 + (z.^2 ./ param.Rl^2));
-strahl = wz(linspace(0, 1e-2, 100));
+strahl = wz(linspace(0, 1e-2, diskreteQuiver));
 
-[x,z] = meshgrid(linspace(-max(strahl), max(strahl), 40), linspace(0, 1e-2, 40));
+rz = @(z) z .* (1 + (param.Rl.^2 ./ z.^2));
+radiusWelle = rz(linspace(1e-9, 1e-2, anzWellen));
+dz = linspace(0, 1e-2, anzWellen);
+dz = dz(2);
 
-Xpoynt = reshape(x, [1, 40*40]);
-Ypoynt = zeros(1,40*40);
-Zpoynt = reshape(z, [1, 40*40]);
+[x,z] = meshgrid(linspace(-max(strahl), max(strahl), diskreteQuiver), linspace(0, 1e-2, diskreteQuiver));
+
+Xpoynt = reshape(x, [1, diskreteQuiver*diskreteQuiver]);
+Ypoynt = zeros(1,diskreteQuiver*diskreteQuiver);
+Zpoynt = reshape(z, [1, diskreteQuiver*diskreteQuiver]);
 
 [pvecNormiert, intensity] = calcPoynting([Xpoynt; Ypoynt; Zpoynt], param);
+
+
+ang = linspace(0, pi, diskreteWelle);
+xp = zeros(length(radiusWelle), diskreteWelle);
+yp = zeros(length(radiusWelle), diskreteWelle);
+for i = 1:length(radiusWelle)
+    xp(i,:) = (radiusWelle(i) .* cos(ang));
+    yp(i,:) = (radiusWelle(i) .* sin(ang)) - radiusWelle(i) + i*dz;
+end
 
 figure;
 quiver(Xpoynt, Zpoynt, pvecNormiert(1,:), pvecNormiert(3,:), '*')
 hold on
-plot(strahl, linspace(0, 1e-2, 100), 'g', 'LineWidth', 1.5)
-plot(-strahl, linspace(0, 1e-2, 100), 'g', 'LineWidth', 1.5)
+plot(strahl, linspace(0, 1e-2, diskreteQuiver), 'g', 'LineWidth', 1.5)
+plot(-strahl, linspace(0, 1e-2, diskreteQuiver), 'g', 'LineWidth', 1.5)
+hold on
+for i = 2:size(xp, 1)
+    plot(xp(i,:), yp(i,:), 'r')
+end
+daspect([1 1 1])
 hold off
