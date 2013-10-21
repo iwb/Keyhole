710650c46b767abb0a46953ff546e32d3621228e
Julius F. Heins <jfheins@gmail.com>
2013-10-21 23:03:18 +0200

    Liste durch Vektor ersetzt, plotdata entfernt

--- keyhole_z.m
+++ keyhole_z.m
@@ -16,16 +16,18 @@ alpha0 = ((vhp1 - vhp2)^2 + versatz^2) / (2 * (vhp1 - vhp2)) / param.w0;
 %% Skalierung und Diskretisierung
 
 % Diskretisierung der z-Achse
-dz = -1e-6;
+dz = -5e-6;
 d_zeta = dz / param.w0;
 
-Apex = List();
-Apex.EnsureCapacity(1000);
-Apex.Add(A0);
+% Blechdicke
+max_z = 5e-3;
+max_zindex = ceil(max_z/-dz);
 
-Radius = List();
-Radius.EnsureCapacity(1000);
-Radius.Add(alpha0);
+Apex = NaN(max_zindex, 1);
+Apex(1) = A0;
+
+Radius = NaN(max_zindex, 1);
+Radius(1) = alpha0;
 
 %% Variablen für die Schleife
 zeta = 0;
@@ -35,83 +37,66 @@ zindex = 0;
 currentA = A0;
 currentAlpha = alpha0;
 
-% Plotdaten
-plotdata = struct();
-plotdata.Apex = Apex;
-plotdata.Radius = Radius;
-plotdata.Angle = List();
-plotdata.Intensity = List();
-plotdata.HeatFlow = List();
-plotdata.Fresnel = List();
-plotdata.z_axis = 0;
-
-plotdata.Angle.Add(NaN);
-plotdata.Intensity.Add(NaN);
-plotdata.HeatFlow.Add(NaN);
-plotdata.Fresnel.Add(NaN);
-
 %% Schleife über die Tiefe
 while (true)
-    
-    zindex = zindex + 1;
-    prevZeta = zeta;
-    zeta = zeta + d_zeta;
-    
-    %% Nullstellensuche mit MATLAB-Verfahren
-    % Variablen für Nullstellensuche
-    arguments = struct();
-    arguments.prevZeta = prevZeta;
-    arguments.zeta = zeta;
-    arguments.prevApex = currentA;
-    arguments.prevRadius = currentAlpha;
-    
-    % Berechnung des neuen Scheitelpunktes
-    func1 = @(A) khz_func1(A, arguments, param, []);
-    currentA = fzero(func1, currentA);
-    
-    % Abbruchkriterium
-    if(isnan(currentA))
-        fprintf('Abbruch weil Apex = Nan. Endgültige Tiefe: %3.0f\n', zeta);
-        break;
-    end
-    if(currentA < -3)
-        fprintf('Abbruch, weil Apex < -3. Endgültige Tiefe: %3.0f\n', zeta);
-        break;
-    end
-    
-    % Berechnung des Radius
-    func2 = @(alpha) khz_func2(alpha, currentA, arguments, param, []);
-    alpha_interval(1) = 0.5*currentA; % Minimalwert
-    alpha_interval(2) = 1.05 * currentAlpha; % Maximalwert
-    currentAlpha = fzero(func2, alpha_interval);
-    
-    % Abbruchkriterium
-    if(isnan(currentAlpha))
-        fprintf('Abbruch weil Radius=Nan. Endgültige Tiefe: %3.0f\n', zeta);
-        break;
-    end
-    if (currentAlpha < 1e-12)
-        fprintf('Abbruch weil Keyhole geschlossen. Endgültige Tiefe: %3.0f\n', zeta);
-        break;
-    end
-    if (zindex > 10 && currentAlpha > arguments.prevRadius)
-        fprintf('Abbruch weil Radius steigt / KH geschlossen. Endgültige Tiefe: %3.0f\n', zeta);
-        break;
-    end
-    if (zindex * dz <= -5e-3)
-        fprintf('Abbruch weil Blechtiefe erreicht.\n');
-        break;
-    end
-    
-    % Plotdata befüllen lassen
-    khz_func1(currentA, arguments, param, plotdata);
-    % Plotdata befüllen lassen
-    khz_func2(currentAlpha, currentA, arguments, param, plotdata);
-    
-    % Werte übernehmen und sichern
-    Apex.Add(currentA);
-    Radius.Add(currentAlpha);
+	
+	zindex = zindex + 1;
+	prevZeta = zeta;
+	zeta = zeta + d_zeta;
+	
+	%% Nullstellensuche mit MATLAB-Verfahren
+	% Variablen für Nullstellensuche
+	arguments = struct();
+	arguments.prevZeta = prevZeta;
+	arguments.zeta = zeta;
+	arguments.prevApex = currentA;
+	arguments.prevRadius = currentAlpha;
+	
+	% Berechnung des neuen Scheitelpunktes
+	func1 = @(A) khz_func1(A, arguments, param, []);
+	currentA = fzero(func1, currentA);
+	
+	% Abbruchkriterium
+	if(isnan(currentA))
+		fprintf('Abbruch weil Apex = Nan. Endgültige Tiefe: %3.0f\n', zeta);
+		break;
+	end
+	if(currentA < -5)
+		fprintf('Abbruch, weil Apex < -5. Endgültige Tiefe: %3.0f\n', zeta);
+		break;
+	end
+	
+	% Berechnung des Radius
+	func2 = @(alpha) khz_func2(alpha, currentA, arguments, param, []);
+	alpha_interval(1) = 0.5*currentA; % Minimalwert
+	alpha_interval(2) = 1.05 * currentAlpha; % Maximalwert
+	currentAlpha = fzero(func2, alpha_interval);
+	
+	% Abbruchkriterium
+	if(isnan(currentAlpha))
+		fprintf('Abbruch weil Radius=Nan. Endgültige Tiefe: %3.0f\n', zeta);
+		break;
+	end
+	if (currentAlpha < 1e-12)
+		fprintf('Abbruch weil Keyhole geschlossen. Endgültige Tiefe: %3.0f\n', zeta);
+		break;
+	end
+	if (zindex > 10 && currentAlpha > arguments.prevRadius)
+		fprintf('Abbruch weil Radius steigt / KH geschlossen. Endgültige Tiefe: %3.0f\n', zeta);
+		break;
+	end
+	if (zindex >= max_zindex)
+		fprintf('Abbruch weil Blechtiefe erreicht.\n');
+		break;
+	end
+	
+	% Werte übernehmen und sichern
+	Apex(zindex) = currentA;
+	Radius(zindex) = currentAlpha;
 end
+
+Apex = Apex(1:zindex-1);
+Radius = Radius(1:zindex-1);
 
 fprintf('Endgültige Tiefe: z=%5.0fµm\n', zeta*param.w0*1e6);
 
