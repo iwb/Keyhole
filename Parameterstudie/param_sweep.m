e78a82f9d46763f73aa1eee10d179e94cb937d10
MarkusSchweier <markus.schweier@googlemail.com>
2014-09-21 13:10:24 +0200

    Kosmetik in der Textausgabe

--- Parameterstudie/param_sweep.m
+++ Parameterstudie/param_sweep.m
@@ -1,4 +1,5 @@
 % Skript zur Durchführung einer Parameterstudie für die Keyholeberechnung
+clear all; close all; clc;
 %% Initialisierung und Festlegung der Bereiche
 addpath 'C:\Users\Markus\Documents\Dissertation\Berechnungen\Keyhole'
 fmax = 2000; % [Hz]
@@ -8,36 +9,33 @@ vmax = sqrt(vsmax^2+(2*pi*fmax*Amax)^2+4*pi*fmax*Amax*vsmax); % [m/s]
 % Auflösung in z-Richtung
 dz = 20; % [µm]
 % Bereiche
-v = 1/60:10/60:vmax; % [m/s]
-P = 1000:25:3000; % [W]
-% Parallelverarbeitung
-poolobj = parpool(1);
+v = [5/60 6/60];%1/60:10/60:vmax; % [m/s]
+P = [2000 3000]; %1000:25:3000; % [W]
 % Vorbelegung
 run('init.m');
 KH = cell(length(v), length(P));
 %% Zyklischer Aufruf des Keyhole-Modells
 for i = 1:length(v)
-    parfor j = 1:length(P)
+    for j = 1:length(P)
         % Parameterfestlegung
         param.scaled.Pe = param.scaled.Pe/param.v*v(i);
         param.v = v(i);
         param.P = P(j);   
         % Parameter ausgeben
-        if poolobj.NumWorkers == 1
-            fprintf('==========================================\n');s
-            fprintf('Vorschub: %0.1f m/s, Leistung: %0.0f W\n', param.v, param.P);
-            fprintf('==========================================\n');
-        end
+        fprintf('==========================================\n');
+        fprintf('Berechnung %0.0f von %0.0d\n', (i-1)*length(P)+j, length(P)*length(v));
+        fprintf('Vorschub: %0.2f m/s, Leistung: %0.0f W\n', param.v, param.P);
+        fprintf('==========================================\n');
         % Keyhole berechnen
         [KH_geom, Reason] = calcKeyhole(dz);
         KH{i,j}.geom = KH_geom;
         KH{i,j}.reason = Reason;
-        KH{i,j}.t = 
+        KH{i,j}.t = min(KH_geom(1,:));
+        KH{i,j}.b = KH_geom(3,1);
     end
 end
 
 %% Shutdown und Speichern
-delete(poolobj);
-clear i j KH_geom Reason fmax Amax vsmax dz
-save(KH_sweep.mat);
+clear i j KH_geom Reason fmax Amax vsmax dz vmax param
+save('KH_sweep.mat');
 
